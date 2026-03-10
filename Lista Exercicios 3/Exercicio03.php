0e67e18b6d8ed0dc964bc2c79d8f14700cc37f99
bruno <venturin_bruno@hotmail.com>
2026-03-10 15:16:08 -0300

    Commit1_L3_100326

---
+++ Lista Exercicios 3/Exercicio03.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 3</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 3</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="numero1" class="form-label">Informe o primeiro número </label>
+              <input type="number" id="numero1" name="valor1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="numero2" class="form-label">Informe o segundo número</label>
+              <input type="number" id="numero2" name="valor2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $valor1 = $_POST["valor1"];
+        $valor2 = $_POST["valor2"];
+
+        if($valor1 > $valor2){
+            echo"$valor2 , $valor1";
+        }
+        elseif($valor1 < $valor2){
+            echo"$valor1 , $valor2";
+        }
+        else{
+            echo "Números iguais: $valor1";
+        }
+
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
