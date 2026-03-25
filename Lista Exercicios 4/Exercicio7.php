ca3e754cd614557fbee31db0a84ba78ae7cb9eb6
bruno <venturin_bruno@hotmail.com>
2026-03-25 16:23:26 -0300

    commit 4

--- Lista Exercicios 4/Exercicio7.php
+++ Lista Exercicios 4/Exercicio7.php
@@ -4,33 +4,35 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 06</title>
+    <title>Exercício 07</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 06</h1>
+        <h1>Exercício 07</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="num" class="form-label">Informe um número:</label>
-                <input type="number" id="num" name="num" step="any" class="form-control" required="">
+                <label for="palavra" class="form-label">Informe uma palavra:</label>
+                <input type="text" id="palavra" name="palavra" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
-        <?php 
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $num = $_POST["num"];
-                $cima = ceil($num);
-                $baixo = floor($num);   
-                $normal = round($num);
+        <?php
 
-                echo "Número original: $num<br>";
-                echo "Arredondado para cima: $cima<br>";
-                echo "Arredondado para baixo: $baixo<br>";
-                echo "Arredondado normalmente: $normal";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $palavra = $_POST["palavra"];
+
+            $invertida = "";
+
+            $tamanho = mb_strlen($palavra, 'UTF-8');
+
+            for ($i = $tamanho - 1; $i >= 0; $i--) {
+                $invertida .= mb_substr($palavra, $i, 1, 'UTF-8');
             }
-        
+
+            echo "Palavra invertida: $invertida";
+        }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
