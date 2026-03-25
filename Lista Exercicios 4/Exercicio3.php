abd75d223bb1fc054ac17f13c9345a30c6f1cb51
bruno <venturin_bruno@hotmail.com>
2026-03-25 12:40:26 -0300

    commit exemplo_projeto e exercicios

--- Lista Exercicios 4/Exercicio3.php
+++ Lista Exercicios 4/Exercicio3.php
@@ -4,27 +4,36 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 01</title>
+    <title>Exercício 03</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercício 01</h1>
+        <h1>Exercício 03</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="palavra" class="form-label">Informe uma palavra:</label>
                 <input type="text" id="palavra" name="palavra" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="palavra2" class="form-label">informe outra palavra:</label>
+                <input type="text" id="palavra2" name="palavra2" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $palavra = $_POST["palavra"];
-            $qtd = mb_strlen($palavra, 'UTF-8');
-            echo "A palavra '$palavra' tem '$qtd' caracteres";
+            $palavra2 = $_POST["palavra2"];
+            if (mb_strpos($palavra1, $palavra2) !== false) {
+                echo "A segunda palavra está contida na primeira.\n";
+            } else {
+                echo "A segunda palavra NÃO está contida na primeira.\n";
+            }
         }
         ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
